b6ab30eab175c9a482b7ff51082adeb983ee945f
sinigam1192 <yuie7893@gmail.com>
2021-01-27 18:41:43 +0900

    フェイドイン追加

--- docs/index.js
+++ docs/index.js
@@ -10,16 +10,47 @@ $(function(){
     });
 
     // フェイドイン
-    $(window).scroll(function () {
-        const wHeight = $(window).height();
-        const scrollAmount = $(window).scrollTop();
-        $('.scrollanime').each(function () {
-            const targetPosition = $(this).offset().top;
-            if(scrollAmount > targetPosition - wHeight + 60) {
-                $(this).addClass("fadeInDown");
+    // $(window).scroll(function () {
+    //     const wHeight = $(window).height();
+    //     const scrollAmount = $(window).scrollTop();
+    //     $('.scrollanime').each(function () {
+    //         const targetPosition = $(this).offset().top;
+    //         if(scrollAmount > targetPosition - wHeight + 60) {
+    //             $(this).addClass("fadeInDown");
+    //         }
+    //     });
+    // });
+
+    var effect_pos = 300; // 画面下からどの位置でフェードさせるか(px)
+    var effect_move = 50; // どのぐらい要素を動かすか(px)
+    var effect_time = 1500; // エフェクトの時間(ms) 1秒なら1000
+
+    // フェードする前のcssを定義
+    $('.scroll-fade').css({
+        opacity: 0,
+        transform: 'translateY('+ effect_move +'px)',
+        transition: effect_time + 'ms'
+    });
+
+    // スクロールまたはロードするたびに実行
+    $(window).on('scroll load', function(){
+        var scroll_top = $(this).scrollTop();
+        var scroll_btm = scroll_top + $(this).height();
+        effect_pos = scroll_btm - effect_pos;
+
+        // effect_posがthis_posを超えたとき、エフェクトが発動
+        $('.scroll-fade').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( effect_pos > this_pos ) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
             }
         });
     });
+
+
 
     //　popber 
     $(function () {
@@ -31,7 +62,7 @@ $(function(){
         $("#skill-content").html("<h2>html, CSS, javascript</h2><div class='row'><p class='col-4 offset-2 skill-centers centers'>html,css,javascriptを使用しwebサイトを作成することができます。</p>                            <i class='fas fa-code col-4 skill-centers-icon skill-centers centers'></i></div>");
     });
     $("#teach").click( function() {
-        $("#skill-content").html("<h2>プログラミング講座</h2><div class='row'><p class='col-4 offset-2 skill-centers centers'>プログラミングに関すること、ITリテラシーに関する内容で講座を実施しています。</p><i class='fas fa-chalkboard-teacher col-4 skill-centers-icon skill-centers centers'></i></div>");
+        $("#skill-content").html("<h2>プログラミング講座</h2><div class='row'><p class='col-4 offset-2 skill-centers centers'>プログラミングに関すること、ITリテラシーに関する内容で講座をできます。</p><i class='fas fa-chalkboard-teacher col-4 skill-centers-icon skill-centers centers'></i></div>");
     });
     $("#dev").click( function() {
         $("#skill-content").html("<h2>開発</h2><div class='row'><p class='col-4 offset-2 skill-centers centers'>開発全般できます。</p><i class='fas fa-database col-4 skill-centers-icon skill-centers centers'></i></div>");
